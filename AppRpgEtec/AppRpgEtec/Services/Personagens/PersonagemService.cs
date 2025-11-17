25957cecebe0cc58623052851f2c0e0e5f3faded
PaolaCondori <paola.condori@etec.sp.gov.br>
2025-11-17 08:56:09 -0300

    :sparkles:Feat: Adicionar métodos de restaurar e zerar ranking

--- AppRpgEtec/AppRpgEtec/Services/Personagens/PersonagemService.cs
+++ AppRpgEtec/AppRpgEtec/Services/Personagens/PersonagemService.cs
@@ -62,5 +62,28 @@ namespace AppRpgEtec.Services.Personagens
             return ListaPersonagens;
         }
 
+        public async Task<int> PutRestaurarPontosAsync(Personagem p)
+        {
+            string urlComplementar = "/RestaurarPontosVida";
+            var result = await _request.PutAsync(apiUrlBase + urlComplementar, p, _token);
+            return result;
+        }
+
+        public async Task<int> PutZerarRankingAsync(Personagem p)
+        {
+            string urlComplementar = "/ZerarRanking";
+            var result = await _request.PutAsync(apiUrlBase + urlComplementar, p, _token);
+            return result;
+        }
+
+        public async Task<int> PutZerarRankingRestaurarVidasGeralAsync()
+        {
+            string urlComplementar = "/ZerarRankingRestaurarVidas";
+            var result = await _request.PutAsync(apiUrlBase + urlComplementar, new Personagem(), _token);
+            return result;
+        }
+
+
+
     }
 }
--- AppRpgEtec/AppRpgEtec/ViewModels/Personagens/ListagemPersonagemViewModel.cs
+++ AppRpgEtec/AppRpgEtec/ViewModels/Personagens/ListagemPersonagemViewModel.cs
@@ -1,4 +1,5 @@
-﻿using AppRpgEtec.Models;
+﻿using Android.Gms.Common.Apis;
+using AppRpgEtec.Models;
 using AppRpgEtec.Services.Personagens;
 using System;
 using System.Collections.Generic;
@@ -65,8 +66,9 @@ namespace AppRpgEtec.ViewModels.Personagens
                 {
                     personagemSelecionado = value;
 
-                    Shell.Current
-                        .GoToAsync($"cadPersonagemView?pId={personagemSelecionado.Id}");
+                    /*Shell.Current
+                        .GoToAsync($"cadPersonagemView?pId={personagemSelecionado.Id}");*/
+                    _ = ExibirOpcoesAsync(personagemSelecionado);
                 }
             }
         }
@@ -95,7 +97,88 @@ namespace AppRpgEtec.ViewModels.Personagens
             }
         }
 
+        public async Task ExecutarRestaurarPontosPersonagem(Personagem p)
+        {
+            await pService.PutRestaurarPontosAsync(p);
+        }
 
+        public async Task ExecutarZerarRankingPersonagem(Personagem p)
+        {
+            await pService.PutZerarRankingAsync(p);
+        }
+
+        public async Task ExecutarZerarRankingRestaurarVidasGeral()
+        {
+            await pService.PutZerarRankingRestaurarVidasGeralAsync();
+        }
+
+        public async void ProcessarOpcaoRespondidaAsync(Personagem personagem, string result)
+        {
+            if (result.Equals("Editar Personagem"))
+            {
+                await Shell.Current
+                .GoToAsync($"cadPersonagemView?pId={personagem.Id}");
+            }
+            else if (result.Equals("Remover Personagem"))
+            {
+                if (await Application.Current.MainPage.DisplayAlert("Confirmação",
+                $"Deseja realmente remover o personagem {personagem.Nome.ToUpper()}?",
+                "Yes", "No"))
+                {
+                    await RemoverPersonagem(personagem);
+                    await Application.Current.MainPage.DisplayAlert("Informação",
+                    "Personagem removido com sucesso!", "Ok");
+                    await ObterPersonagens();
+                }
+            }
+            else if (result.Equals("Restaurar Pontos de Vida"))
+            {
+                if (await Application.Current.MainPage.DisplayAlert("Confirmação",
+                $"Restaurar os pontos de vida de {personagem.Nome.ToUpper()}?", "Yes", "No"))
+                {
+                    await ExecutarRestaurarPontosPersonagem(personagem);
+                    await Application.Current.MainPage.DisplayAlert("Informação",
+                    "Os pontos foram restaurados com sucesso.", "Ok");
+                    await ObterPersonagens();
+                }
+            }
+            else if (result.Equals("Zerar Ranking do Personagem"))
+            {
+                if (await Application.Current.MainPage.DisplayAlert("Confirmação",
+                $"Zerar o ranking de {personagem.Nome.ToUpper()}?", "Yes", "No"))
+                {
+                    await ExecutarZerarRankingPersonagem(personagem);
+                    await Application.Current.MainPage.DisplayAlert("Informação",
+                    "O ranking foi zerado com sucesso.", "Ok");
+                    await ObterPersonagens();
+                }
+            }
+        }
+
+        public async Task ExibirOpcoesAsync(Personagem personagem)
+        {
+            try
+            {
+                personagemSelecionado = null;
+                string result = string.Empty;
+
+                result = await Application.Current.MainPage
+                    .DisplayActionSheet("Opções para o personagem: " + personagem.Nome,
+                    "Cancelar",
+                    "Editar Personagem",
+                    "Restaurar Pontos de Vida",
+                    "Zerar Ranking do Personagem",
+                    "Remover Personagem");
+
+                if (result != null)
+                    ProcessarOpcaoRespondidaAsync(personagem, result);
+            }
+            catch (Exception ex) 
+            {
+                await Application.Current
+                    .MainPage.DisplayAlert("Ops...", ex.Message, "Ok");
+            }
+        }
 
     }
 }
